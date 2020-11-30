cc357893ebeada6cc9875d1fcb84c007f6361759
chunga <clintonchunga@gmail.com>
2020-11-30 19:21:50 +0100

    strougling to fix my massage and names

--- index.js
+++ index.js
@@ -1,70 +1,75 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
 // creating the array of questions
 
-function promptUser(){
-    return inquirer.prompt([
-        {   type: "input",
-             name: "tittle",
-            massage: "what is the name of this project"
-            
-        },
-        {   type: "input",
-            massage: "Description of this project",
-            name: "Description"
-        },
-        {   type: "input",
-            massage: "what where your motivations for this project",
-            name: "motivations"
-        },
-        {   type: "input",
-            massage: "what are the installation instructions of this project? write N/A if none",
-            name: "installation"
-        },
-        {   type: "input",
-            massage: "how is this application used?",
-            name: "usage"
-        },
-        {   type: "input",
-            massage: "list the contibutors on this project, write N/A if non",
-            name: "contributors"
-        },
-        {   type: "input",
-            massage: "what are the test instructions",
-            name: "test"
-        },
-        {   type: "checkbox",
-            massage: "select a license",
-            choices: [
-                "Apache",
-                "MIT",
-                "ISC",
-                "GNU GPLv3"
-            ],
-            name: "license"
-        },
-        {   type: "input",
-            massage: "whose credit is this work",
-            name: "credits"
-        },
-        {   type: "input",
-            massage: "what is your github user name",
-            name: "username"
-        },
-        {   type: "input",
-            massage: "what is your email",
-            name: "email"
-        }
-    ]);
-}
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "tittle",
+      massage: "what is the name of this project"
+    },
+    {
+      type: "input",
+      massage: "Description of this project",
+      name: "Description"
+    },
+    {
+      type: "input",
+      massage: "what where your motivations for this project",
+      name: "motivations"
+    },
+    {
+      type: "input",
+      massage:"what are the installation instructions of this project? write N/A if none",
+      name: "installation"
+    },
+    { type: "input", 
+    massage: "how is this application used?",
+    name: "usage"
+},
+    {
+      type: "input",
+      massage: "list the contibutors on this project, write N/A if non",
+      name: "contributors"
+    },
+    { type: "input",
+     massage: "what are the test instructions",
+      name: "test"
+     },
+    {
+      type: "checkbox",
+      massage: "select a license",
+      choices: [ 
+          "Apache", 
+          "MIT", 
+          "ISC", 
+          "GNU GPLv3"
+        ],
+      name: "license"
+    },
+    { type: "input", 
+      massage: "whose credit is this work", 
+      name: "credits"
+    },
+    {
+      type: "input",
+      massage: "what is your github user name",
+      name: "username"
+    },
+    { type: "input",
+     massage: "what is your email",
+     name: "email" 
+    }
+  ])
+};
 
-function generateMarkdown(response){
-    return`
+function generateMarkdown(response) {
+  return `
 
 # ${response.tittle}
 
@@ -104,7 +109,7 @@ function generateMarkdown(response){
 ##License:
       ${response.license}
 
-    for more clientInformation,click on the link bellow to find out more 
+    for more Information concernibg the licences,click on the link bellow 
 
     [License](https://opensource.org/License-${response.license})
       
@@ -115,26 +120,22 @@ function generateMarkdown(response){
 
     [Github Profile](https://github.com/${response.username})
 
-    for additional questions,you can reach me at my email at: ${response.email}`
-      
+    for additional questions,you can reach me at my email at: ${response.email}`;
 }
 
 // initializinf function
 
-async function init(){
+async function init() {
+  try {
+    const response = await promptUser();
 
-    try{
-        const response = await promptUser();
+    const readme = generateMarkdown(response);
 
-        const readme = generateMarkdown(response);
-
-        await writeFileAsync("README.md", readme);
-        console.log("success");
-
-    } catch (err) {
-        console.log(err);
-    }
-
+    await writeFileAsync("README.md", readme);
+    console.log("success");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // call to initialize program
